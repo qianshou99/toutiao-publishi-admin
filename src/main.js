78fc3d89fde4b88e5a714931ff4738426ccccf40
qianshou99 <1353806762@qq.com>
2020-04-20 13:16:38 +0800

    项目初始化-调整初始目录结构

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// 加载全局样式
+import './styles/index.less'
+Vue.use(ElementUI)
 Vue.config.productionTip = false
-
+// 创建vue实例
 new Vue({
   router,
   render: h => h(App)
