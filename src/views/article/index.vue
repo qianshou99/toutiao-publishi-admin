7daa898e87414f86cd4cc27a75705746a44fe8e7
qianshou99 <1353806762@qq.com>
2020-04-22 21:54:36 +0800

    内容管理-页面布局（分页组件）

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -38,6 +38,27 @@
     </el-form-item>
    </el-form>
 <!-- /数据筛选表单 -->
+<!-- 数据列表 -->
+     <el-table
+      :data="tableData"
+      stripe
+      style="width: 100%">
+      <el-table-column
+        prop="date"
+        label="日期"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+    </el-table>
+<!-- 数据列表 -->
   </div>
 </template>
 
@@ -57,7 +78,24 @@ export default {
         type: [],
         resource: '',
         desc: ''
-      }
+      },
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
     }
   },
   computed: {},
