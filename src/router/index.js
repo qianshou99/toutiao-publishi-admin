4c6679b6fcf5dfd642496b359a45f3e2aca4995d
qianshou99 <1353806762@qq.com>
2020-04-22 19:37:31 +0800

    路有拦截,导航守卫功能

--- src/router/index.js
+++ src/router/index.js
@@ -32,5 +32,25 @@ const routes = [
 const router = new VueRouter({
   routes
 })
-
+// 导航守卫,路由拦截器
+router.beforeEach((to, from, next) => {
+  // 如果登录了允许通过,如果没有登录,跳转到登录页面
+  // to：要去的路由信息
+  // from：来自哪里的路由信息
+  // next：放行方法
+  if (to.path !== '/login') {
+    const user = JSON.parse(window.localStorage.getItem('user'))
+    // 校验非登录页面的登录状态
+    if (user) {
+      // 如果有token,证明登录了,允许通过
+      next()
+    } else {
+      // 如果没有登录跳转到登录页面
+      next('/login')
+    }
+  } else {
+    // 登录了页面正常允许通过
+    next()
+  }
+})
 export default router
