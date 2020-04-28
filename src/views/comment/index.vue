cce4204f03cb47fe06a44fe123553cddc49a302d
qianshou99 <1353806762@qq.com>
2020-04-28 23:10:32 +0800

    个人设置展示用户资料

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -79,23 +79,7 @@ export default {
   props: {},
   data () {
     return {
-      tableData: [{
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1517 弄'
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1519 弄'
-      }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄'
-      }],
+
       articles: [], // 文章数据列表
       pageSize: 10,
       page: 1, // 当前激活的页码
---
+++ src/views/settings/index.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="settings-container">
+      <el-card class="box-card">
+        <div slot="header" class="clearfix">
+            <!-- 面包屑路径导航 -->
+            <el-breadcrumb separator-class="el-icon-arrow-right">
+            <el-breadcrumb-item to="/">首页</el-breadcrumb-item>
+            <el-breadcrumb-item>个人设置</el-breadcrumb-item>
+            </el-breadcrumb>
+            <!-- /面包屑路径导航 -->
+        </div>
+        <el-row>
+            <el-col :span="15">
+            <el-form ref="form" :model="form" label-width="70px">
+                <el-form-item label="编号">
+                {{ user.id }}
+                </el-form-item>
+                <el-form-item label="手机">
+                {{ user.mobile }}
+                </el-form-item>
+                <el-form-item label="媒体名称">
+                <el-input v-model="user.name"></el-input>
+                </el-form-item>
+                <el-form-item label="媒体介绍">
+                <el-input type="textarea" v-model="user.intro"></el-input>
+                </el-form-item>
+                <el-form-item label="邮箱">
+                <el-input v-model="user.email"></el-input>
+                </el-form-item>
+                <el-form-item>
+                <el-button type="primary" @click="onSubmit">保存</el-button>
+                </el-form-item>
+            </el-form>
+            </el-col>
+            <!-- offset偏移 -->
+            <el-col :offset="2" :span="4">
+            <el-avatar
+                shape="square"
+                :size="150"
+                fit="cover"
+                :src="user.photo"
+            ></el-avatar>
+            <p>点击修改头像</p>
+            </el-col>
+        </el-row>
+    </el-card>
+  </div>
+</template>
+
+<script>
+import { getUserProfile } from '@/api/user'
+export default {
+  name: 'SettingsIndex',
+  components: {},
+  props: {},
+  data () {
+    return {
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      },
+      user: {
+        email: '',
+        id: null,
+        intro: '',
+        mobile: '',
+        name: '',
+        photo: ''
+      } // 用户资料
+    }
+  },
+  computed: {},
+  watch: {},
+  created () {
+    //   初始化数据
+    this.loadUser()
+  },
+  mounted () {},
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    },
+    loadUser () {
+      getUserProfile().then(res => {
+        // console.log(res)
+        this.user = res.data.data
+      })
+    }
+  }
+}
+</script>
+
+<style scoped lang="less"></style>
