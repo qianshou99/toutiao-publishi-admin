240d78dce0459c314f55235efb666d2d61aaa909
qianshou99 <1353806762@qq.com>
2020-04-28 22:20:03 +0800

    数据分页处理完成

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -50,14 +50,20 @@
           </template>
         </el-table-column>
       </el-table>
+       <!--
+        绑定页码
+        绑定每页大小
+        current-page 控制激活的页码，初始肯定是第 1 页
+        page-sizes 控制可选的每页大小
+       -->
       <el-pagination
         @size-change="handleSizeChange"
         @current-change="handleCurrentChange"
-        :current-page="1"
-        :page-sizes="[100, 200, 300, 400]"
-        :page-size="100"
+        :current-page.sync="page"
+        :page-sizes="[10, 20, 30, 40]"
+        :page-size.sync="pageSize"
         layout="total, sizes, prev, pager, next, jumper"
-        :total="400"
+        :total="totalCount"
         background
       >
       </el-pagination>
@@ -90,7 +96,10 @@ export default {
         name: '王小虎',
         address: '上海市普陀区金沙江路 1516 弄'
       }],
-      articles: [] // 文章数据列表
+      articles: [], // 文章数据列表
+      pageSize: 10,
+      page: 1, // 当前激活的页码
+      totalCount: 0 // 总数据条数
     }
   },
   computed: {},
@@ -101,12 +110,21 @@ export default {
   },
   mounted () {},
   methods: {
-    handleSizeChange () {},
-    handleCurrentChange () {},
-    loadArticles () {
+    handleSizeChange (page) {
+      this.loadArticles(1)
+    },
+    handleCurrentChange (page) {
+      // 页码改变，加载指定页码数据
+      this.loadArticles(page)
+    },
+    loadArticles (page = 1) {
+      // 让分页组件激活的页码和请求数据的页码保持一致
+      this.page = page
       // 获取评论数据
       getArticles({
-        response_type: 'comment'
+        response_type: 'comment',
+        page,
+        per_page: this.pageSize
       }).then(res => {
         console.log(res)
         const { results } = res.data.data
@@ -114,6 +132,7 @@ export default {
           article.statusDisabled = false
         })
         this.articles = results
+        this.totalCount = res.data.data.total_count
       })
     },
     // 修改评论状态
